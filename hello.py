861bbaae6bea0d37d712eefe40d2b30f5de2a1bb
Ku22023 <ku22023@my.westlake.school.nz>
2024-06-21 12:00:35 +1200

    number game

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
-num = 1
-while num < 100:
-    num = num + 1
-    if num % 2 == 1:
-        print(f"{num} is odd")
+from random import randint
+x = randint(1,100)
+guesscount = 10
+while guesscount > 0:
+    inp = int(input("Guess a Number 1-100: "))
+    if guesscount > 0:
+        if inp != x:
+            guesscount = guesscount - 1
+            print(f"Wrong guess! You have {guesscount} guesses remaining!")
+            if inp > x:
+                print(f"The number is lower than {inp}!")
+            elif inp < x:
+                print(f"The number is higher than {inp}!")
+        else:
+            print(f"Congratulations! You got the number in {10 - guesscount} guesses!")
+            guesscount = 0
     else:
-        print(f"{num} is even")
+        print("You have no guesses remaining! Please restart")
